429c619c6fdea86f08fdb8ee9615666b96522961
joie <joie_chen9@hotmail.com>
2018-01-23 14:57:04 +0800

    blockchain study

---
+++ Python_learn.py
@@ -0,0 +1,10 @@
+my_list = [1,2,3,4,5]
+my_list1 = ["test",[6,7,8]]
+
+print (my_list[0])
+print (my_list1[0][1])
+print (my_list1[1][2])
+
+print (my_list[1:3])
+print (my_list[:-2])
+print (my_list1[:2][:-1])
---
+++ ex1.py
@@ -0,0 +1,4 @@
+print ("Hello World!")
+print ("This is fun.")
+print ("Yay!Printing.")
+print ("I'd much rather you 'not'.")
